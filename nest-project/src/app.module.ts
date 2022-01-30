ccf8e218d9e8c219c96ea443916a335d628df295
TakayukiTooyama <t.tooyama107@gmail.com>
2022-01-30 15:29:36 +0900

    定期実行処理の追加

--- nest-project/src/app.module.ts
+++ nest-project/src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TasksModule } from './tasks/tasks.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [TasksModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ScheduleModule.forRoot()],
 })
 export class AppModule {}
